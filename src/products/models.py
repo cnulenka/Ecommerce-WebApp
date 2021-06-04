45736a107aae8e2fcbbdfac7d381e4cbb6712f90
cnulenka <sinulenka001@gmail.com>
2021-06-04 21:08:40 +0530

    custom queries, slug field, signals

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,10 @@
 import random
 import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+
+from .utils import unique_slug_generator
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -19,23 +23,64 @@ def upload_image_path(instance, filename):
             final_filename=final_filename
             )
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
 class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self): #Product.objects.featured() 
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id=id) # Product.objects == self.get_queryset()
         if qs.count() == 1:
             return qs.first()
         return None
 
+
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
+    slug            = models.SlugField(blank=True, unique=True)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured        = models.BooleanField(default=False)
+    active          = models.BooleanField(default=True)
 
     objects = ProductManager()
+
+    def get_absolute_url(self):
+        return "/products/{slug}/".format(slug=self.slug)
 
     def __str__(self):
         return self.title
 
     def __unicode__(self):
         return self.title
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
+
+
+
+
+
+
+
+
+
+
